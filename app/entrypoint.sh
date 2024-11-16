a28c6257987da1b000f83e002e4a0886194ad826
dilozy <denfil228@yandex.ru>
2024-11-16 18:20:56 +0300

    Fixed entrypoint procedures

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,21 +3,34 @@
 echo "Waiting for database to be available..."
 /wait-for-it.sh db:3306 --timeout=30 --strict -- echo "Database is ready."
 
-# Применяем миграции, если они не были применены
-if ! python manage.py showmigrations | grep -q ' [ ]'; then
-  echo "Applying migrations..."
-  python manage.py migrate
+
+MIGRATION_FLAG="/app/.migrations_applied"
+
+if [ -f "$MIGRATION_FLAG" ]; then
+    echo "Migrations have already been applied, skipping..."
+else
+    echo "Migrations have not been applied. Applying migrations..."
+    
+    python manage.py migrate
+
+    touch "$MIGRATION_FLAG"
+    echo "Migration flag created: $MIGRATION_FLAG"
 fi
 
-# Загружаем данные, если это не было сделано
-if ! python manage.py loaddata db.json; then
-  echo "Loading fixture..."
-  python -Xutf8 manage.py loaddata db.json
-fi
+FIXTURE_FLAG="/app/.fixture_loaded"
 
+if [ -f "$FIXTURE_FLAG" ]; then
+    echo "Fixture has been already loaded."
+else
+    echo "Loading fixture..."
+    
+    python -Xutf8 manage.py loaddata db.json
+    
+    touch "$FIXTURE_FLAG"
+    echo "Fixture flag created: $FIXTURE_FLAG"
+fi
 
 echo "Checking for static files..."
-# Проверяем, есть ли статические файлы
 if ! find /app/staticfiles -type f | grep -q .; then
   echo "Collecting static files..."
   python manage.py collectstatic --noinput
@@ -25,9 +38,7 @@ else
   echo "Static files already collected."
 fi
 
-# Запускаем Django сервер
 echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
 
-# Выполняем все переданные параметры (если они есть)
 exec "$@"
